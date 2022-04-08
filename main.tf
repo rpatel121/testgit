240a4ffcbe45a5bd6b1a733c4a6c1526aed4dbfb
EC2 Default User <ec2-user@ip-172-31-91-81.ec2.internal>
2022-04-08 06:01:05 +0000

    public IP address and AZ zone added

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ resource "aws_instance" "my-instance" {
   ami             = data.aws_ssm_parameter.ami_id.value
   subnet_id       = "subnet-086347872eb823260"
   instance_type   = "t2.micro"
+  associate_public_ip_address = "true"
+  availability_zone = "us-east-1f"
   vpc_security_group_ids = ["sg-06c51062474fb4ba7"]
   tags = {
     Name = "Terraform Instance"
